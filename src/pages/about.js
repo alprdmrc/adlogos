cace95e2c185552c8e198c09c0b3decc74231404
Alper Demirci <alprdmrc20@gmail.com>
2022-11-16 11:47:25 +0300

    baby progress

---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Adlogos">
+            <p>Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip 
+                ex ea commodo consequat. Duis aute irure dolor 
+                in reprehenderit in voluptate velit esse cillum dolore 
+                eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </Layout>
+    )
+}
+
+export const Head = () => <title>About Logos</title>
+
+export default AboutPage
